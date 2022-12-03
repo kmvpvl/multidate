250567f9e1e64ec37a5db28a2ef7759cfd1a5509
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2022-12-03 13:48:58 +0300

    ready prototype with examples

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -1,44 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import * as MultiDate from 'scheduling-multidate';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MultiDate from 'scheduling-multidate';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 const tmd = {
-    title: 'TEST',
-    estimated: {
-      datepoint: new Date('2022-07-11T05:00:00.000+00:00'),
-      tolerance: {
-        left: 10,
-        right: 0,
-      },
+  title: 'TEST',
+  estimated: {
+    datepoint: new Date('2022-07-11T05:00:00.000+00:00'),
+    tolerance: {
+      left: 10,
+      right: 0,
     },
-    baseline: new Map([
-      [
-        '0',
-        {
-          datepoint: new Date('2022-07-11T05:00:00.000+00:00'),
-          tolerance: {
-            left: 1,
-            right: 5,
-          },
+  },
+  baseline: new Map([
+    [
+      '0',
+      {
+        datepoint: new Date('2022-07-11T05:00:00.000+00:00'),
+        tolerance: {
+          left: 1,
+          right: 5,
         },
-      ],
-      [
-        'YTDQ3',
-        {
-          datepoint: new Date('2022-07-11T05:00:00.000+00:00'),
-          tolerance: {
-            left: 0,
-            right: 0,
-          },
+      },
+    ],
+    [
+      'YTDQ3',
+      {
+        datepoint: new Date('2022-07-11T05:00:00.000+00:00'),
+        tolerance: {
+          left: 0,
+          right: 0,
         },
-      ],
-    ]),
-  };
+      },
+    ],
+  ]),
+};
 
+let tmd1:any;
+//Object.assign(tmd1, tmd);
+//tmd1.
 root.render(
-      
   <React.StrictMode>
-    <MultiDate {...tmd} />
+    Brief exterior
+    <MultiDate title={"BRIEF"} 
+      estimated={tmd.estimated} 
+      baseline={tmd.baseline}
+      state={{style:"brief", showTolerance:false, showTime:false}} />
   </React.StrictMode>,
 )
